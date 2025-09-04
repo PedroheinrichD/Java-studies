47ba52d056b4e0d7db5bc12dfd12219809f30c8d
Pedro Heinrich <pedroheinrich91@gmail.com>
2025-09-04 10:30:32 -0300

    Update nas anotações JAVA
    Adicionada condição if e else

--- main.java
+++ main.java
@@ -1,6 +1,41 @@
-// A base da linguagem Java (POO e Fortemente tipada)
-public class main{}
+/*
+public → significa que esse método pode ser acessado de qualquer lugar. 
+A JVM precisa “enxergar” ele, por isso é público.
 
+static → quer dizer que esse método pertence à classe e não a um objeto. Ou seja, 
+você não precisa criar um objeto da classe Main pra rodar, ele já está pronto.
+
+void → tipo de retorno. Aqui é void porque o método main não devolve nada, só executa (retorno).
+
+main → nome fixo do método. Tem que ser exatamente assim, senão não roda.
+
+String[] args → é um array de Strings que serve pra receber argumentos de linha de comando
+ (tipo passar valores quando você executa o programa). Se não usar, pode deixar quieto
+  
+System.out.println(); --> ira exibir a string, int , etc
+ */
+
+
+public class Main{
+    public static void main(String[] args){ 
+        int idade = 19;
+        System.out.println("Idade: " + idade);
+
+
+
+
+        if (idade > 18) { // condição 
+            System.out.println("Voce é maior de idade!"); // printar mensagem caso a condição seja verdadeira
+        }else{
+            System.out.println("Voce é menor de idade!");// caso contrario a condição seja falsa
+        }
+
+
+
+
+    }
+
+}
 
 /* Criação de Váriaveis -->  TIPO nomeVariavel = VALOR
 
@@ -9,7 +44,7 @@ public class main{}
 */
 
 
-/* tipos primitivos 
+/* [tipos primitivos] 
 
     byte -> 8 bits -> -128 a 127
     short -> 16 bits -> -32.768 a 32.767
@@ -25,3 +60,18 @@ public class main{}
     boolean -> true ou false
 
 */
+
+
+/*  [CONDICIONAIS]
+    
+
+*/
+
+
+
+
+
+
+
+
+

c80e94a79de565b9fc3afc3f07c31f0f69f3bef7
Pedro Heinrich <pedroheinrich91@gmail.com>
2025-09-04 09:52:52 -0300

    Adiciona primeiras anotações sobre tipos primitivos em Java

---
+++ main.java
@@ -0,0 +1,27 @@
+// A base da linguagem Java (POO e Fortemente tipada)
+public class main{}
+
+
+/* Criação de Váriaveis -->  TIPO nomeVariavel = VALOR
+
+    int = tipo inteiro --> int idade = 19
+    string= tipo texto --> string nome = "Pedro" / string -> representa palavras e frases --> "aspas duplas" 
+*/
+
+
+/* tipos primitivos 
+
+    byte -> 8 bits -> -128 a 127
+    short -> 16 bits -> -32.768 a 32.767
+    int -> 32 bits -> -2.147.483.648 a 2.147.483.647
+    long -> 64 bits -> -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+
+    float -> 32bits -> precisão simples -> 5.50 (NUMEROS DECIMAIS)
+    double -> 64 bits -> precisão dupla 
+
+    
+    char -> representar um unico caractere -> 'a', 'b', 'c'  --> 'aspas simples'
+
+    boolean -> true ou false
+
+*/

b9f5bd330b8cdeaef7ecc7e10ee725de3ad391b8
Pedro Heinrich <pedroheinrich91@gmail.com>
2025-09-18 07:07:32 -0300

    Adicionada Exercicios sobre propriedades
    -Adicionada informação que imprime o sistema operacional que está sendo utilizado
    
    -Adicionada informação que imprime qual linguagem está sendo usada

--- Es.java
+++
@@ -1,7 +0,0 @@
-public class Es{
-    public static void main(String[] args){
-
-        
-
-    }
-}
--- Guanabara/primeiroPrograma.java
+++ Guanabara/primeiroPrograma.java
@@ -1,8 +1,22 @@
-public class primeiroPrograma{ 
-    public static void main(String[] args){ // metodo principalç (main)
-        System.out.print("Olá mundo!");
+import java.util.Date; // importando uma biblioteca java
+
+public class primeiroPrograma {
+    public static void main(String[] args) { // metodo principalç (main)
+        Date data = new Date(); // isso cria uma data
+        System.out.println("Hoje é " + data.toString()); // imprime a data atual , transformando em string
         
+
+        // verificar o idioma do sistema "exercicio" ✓
+        System.getProperties(); // imprime as propriedades do sistema --> .getProperties() --> método
+        String windows = System.getProperty("os.name"); // Criando a variável do tipo string e imprimindo o sistema operacional (os.name → retorna o nome do sistema operacional.)
+        System.out.println("Sistema operacional: " + windows.toString()); // imprime o sistema operacional que está sendo utilizado
+
+
+
+        // verificar a resolução de do sistema "exercicio" ✓
+        String language = System.getProperty("user.language");
+        System.out.println("Liguagem original do sistema: " + language.toString());
+
 
     }
 }
-
